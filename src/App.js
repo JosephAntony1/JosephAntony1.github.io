22d798fcdcec251a2cd00dda5703d267c6eedd04
jantony1 <jantony789@gmail.com>
2019-05-17 11:04:28 -0400

    Best practices

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import logo from "./JA Logo.svg";
-import Header from "./Header";
+import Header from "./components/Header";
 import { PrimaryButton } from "office-ui-fabric-react";
 import Typing from 'react-typing-animation';
 
