2d40467e46d529d9acff9cc091fb6369c5e3542c
jantony1 <jantony789@gmail.com>
2019-05-17 11:03:44 -0400

    Best practices

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import './App.css';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import logo from './JA Logo.svg';
+import React from "react";
+import "./App.css";
+import logo from "./JA Logo.svg";
+import Header from "./Header";
+import { PrimaryButton } from "office-ui-fabric-react";
+import Typing from 'react-typing-animation';
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <img src={logo} className="App-logo" alt="logo" />
+      <body class="ms-Fabric" dir="ltr">
+        <span class="ms-font-su ms-fontColor-themePrimary">Big blue text</span>
+      </body>
+      <PrimaryButton>Sup</PrimaryButton>
 
-      <Typography variant="h1" color="inherit">
-        Hello.
-      </Typography>
-      <Button variant="contained" color="primary">
-        <Typography variant="h6" color="inherit">
-          Hello World.
-        </Typography>
-      </Button>
+      <Typing>
+        <div>
+          There will be a 1000ms delay here,
+          <Typing.Delay ms={1000} />
+          then this will be typed.
+        </div>
+      </Typing>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Header() {
+  return (
+    <div className="header">
+    </div>
+  );
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default Header;
