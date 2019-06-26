b93834cdab1b227ccb7e2e474e6d714fcb23816d
jantony1 <jantony789@gmail.com>
2019-06-26 01:16:30 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ import { CSSTransitionGroup } from "react-transition-group"; // ES6
 import Fade from "react-reveal/Fade";
 import ScrollableAnchor from "react-scrollable-anchor";
 import { StickyContainer, Sticky } from "react-sticky";
-import { ParallaxProvider, ParallaxBanner, Parallax} from "react-scroll-parallax";
+import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
 import BG from "./images/IMG_3336.jpg";
 import FG from "./images/arlington.png";
 import Logo from "./JA_Logo.svg";
 import styles from "./ParallaxBanner.scss";
-import "./styles.scss";
+import Plx from "react-plx";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.scrollPane = React.createRef();
+    this.state = { width: 0, height: 0, margin: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
   render() {
     const layers = [
       {
@@ -33,6 +46,8 @@ class App extends React.Component {
       <ParallaxProvider>
         <Fade>
           <div className="App">
+            <link rel="stylesheet" href="fabric.css" />
+
             <div className="vertical">
               <div className={styles.bannerContainer}>
                 <ParallaxBanner
@@ -40,26 +55,34 @@ class App extends React.Component {
                   layers={layers}
                   style={{ height: "100vh" }}
                 >
-                  <span
-                    className="ms-fontSize-68  parallaxChildren"
-                    style={{ color: "white" }}
-                  >
-                    <img
-                      src={Logo}
-                      alt={Logo}
-                      style={{ height: "50vh", fill: "green" }}
-                    />
+                  <span className="ms-fontSize-68 parallaxChildren">
+                    <img src={Logo} alt={Logo} style={{ height: "50vh" }} />
 
-                    <h1 style={{ color: "black" }}>Joseph Antony</h1>
-                    <h5>An interactive resume</h5>
+                    <h1 style={{ color: "#750b1c" }}>Joseph Antony</h1>
+                    <h5 style={{ color: "#460711" }}>An interactive resume</h5>
                   </span>
                 </ParallaxBanner>
-                <Parallax>
-                  <img src={Logo} alt={Logo} style={{ height: "20vh" }} />
-                </Parallax>
-
               </div>
             </div>
+            <Plx
+              parallaxData={[
+                {
+                  start: this.state.height - 500,
+                  end: 700,
+                  duration: "10%",
+                  properties: [
+                    {
+                      startValue: 0,
+                      endValue: -this.state.width + this.state.width * 0.7,
+                      property: "translateX"
+                    }
+                  ]
+                }
+              ]}
+            >
+              <img src={Logo} alt={Logo} style={{ height: "20vh" }} />
+            </Plx>
+            <img src={Logo} alt={Logo} style={{ height: "100vh" }} />
           </div>
         </Fade>
       </ParallaxProvider>
