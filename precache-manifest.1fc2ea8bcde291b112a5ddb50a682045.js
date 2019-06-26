4c4c100032df56f87ee9404d988a1ab3a5e929f1
jantony1 <jantony789@gmail.com>
2019-06-25 23:47:52 -0400

    Updates

---
+++ precache-manifest.1fc2ea8bcde291b112a5ddb50a682045.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30656144a2f411c3a9f4718ade81e4d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "572087365d2aa9e1cdf1",
+    "url": "/static/css/main.e17a6f70.chunk.css"
+  },
+  {
+    "revision": "c6823f96fce79747a5ea",
+    "url": "/static/js/2.0a8b17cd.chunk.js"
+  },
+  {
+    "revision": "572087365d2aa9e1cdf1",
+    "url": "/static/js/main.df0c4981.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5b68806853b141e032e0d01fca7046f5",
+    "url": "/static/media/IMG_3336.5b688068.jpg"
+  },
+  {
+    "revision": "6284c2a807339f97618415e091230803",
+    "url": "/static/media/JA_Logo.6284c2a8.svg"
+  },
+  {
+    "revision": "39d3b64007a500704b100aa310feb146",
+    "url": "/static/media/arlington.39d3b640.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.798e89c34fee044fa3872e22702584c1.js"
+  "/precache-manifest.1fc2ea8bcde291b112a5ddb50a682045.js"
 );
 
 self.addEventListener('message', (event) => {
