0bb9412d33c0d5fb03f7822d4551374ead37061b
jantony1 <jantony789@gmail.com>
2019-06-28 03:29:51 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27a98a4d2d03c18eaf65d4cd8ee09314.js"
+  "/precache-manifest.d65a61f5af29701fa551fcc65866bf78.js"
 );
 
 self.addEventListener('message', (event) => {
